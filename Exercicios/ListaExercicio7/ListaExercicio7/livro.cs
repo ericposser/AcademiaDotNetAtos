61f79d4f4da6803de09abe85031a2468e4e1ae74
Eric Posser <eric_alegrete2002@hotmail.com>
2022-10-08 17:52:23 -0300

    exercicios

---
+++ Exercicios/ListaExercicio7/ListaExercicio7/livro.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaExercicio7
+{
+    internal class livro
+    {
+        private string titulo;
+        private string autor;
+        private int numeroPaginas;
+        public livro(string titulo, string autor, int numeroPaginas)
+        {
+            this.titulo = titulo;
+            this.autor = autor;
+            this.numeroPaginas = numeroPaginas;
+        }
+    }
+
+}
---
+++ Exercicios/ListaExercicio7/ListaExercicio7/pessoa.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaExercicio7
+{
+    internal class pessoa
+    {
+        private string nome;
+        private float altura;
+        private float peso;
+        private int idade;
+
+        public pessoa(string? nome, int idade, float peso, float altura)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.peso = peso;
+            this.altura = altura;
+        }
+
+        public void exibirDados()
+        {
+            Console.WriteLine("Nome: " + nome);
+            Console.WriteLine("Altura: " + altura);
+            Console.WriteLine("Peso: " + peso);
+            Console.WriteLine("Idade: " + idade);
+        }
+    }
+}
