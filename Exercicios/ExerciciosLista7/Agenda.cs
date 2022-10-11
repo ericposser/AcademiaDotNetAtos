d6fc447aec3b550e3844b1c6ebaea72abc327bf7
Eric Posser <eric_alegrete2002@hotmail.com>
2022-10-11 15:45:41 -0300

    exercicios

---
+++ Exercicios/ExerciciosLista7/Agenda.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosLista7
+{
+    internal class Agenda
+    {
+        public List<Pessoa> pessoas = new List<Pessoa>();
+        public String nome;
+        public Agenda(string nome)
+        {
+            this.nome = nome;
+            //carros = new List<Carro>();
+        }
+        //public int[] vet = new int[10];
+        public void ArmazenaPessoa(String nome, int idade, float altura)
+        {
+            Pessoa p = new Pessoa(nome, idade, altura);
+            pessoas.Add(p);
+
+        }
+        public Pessoa BuscaPessoa(String nome)
+        {
+            Pessoa r = null;
+            foreach (Pessoa p in pessoas)
+            {
+                if (p.nome.Equals(nome))
+                {
+                    r = p;
+                }
+            }
+            return r;
+
+        }
+        public void MostraPessoas()
+        {
+            /*for(int i = 0; i < carros.Count; i++)
+            {
+                Console.WriteLine("Carro "+i+": " + carros[i].modelo);
+            }*/
+
+            foreach (Pessoa p in pessoas)
+            {
+                Console.WriteLine("Nome: " + p.nome + " Idade: " + p.idade + " Altura: " + p.altura);
+            }
+        }
+        public void RemovePessoa(String nome)
+        {
+            foreach (Pessoa p in pessoas)
+            {
+                if (p.nome.Equals(nome))
+                {
+                    //aqui devo apagar o carro da lista
+                }
+            }
+        }
+    }
+}
---
+++ Exercicios/ExerciciosLista7/Produto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosLista7
+{
+    internal class Produto
+    {
+        private int qtdProduto;
+        private string nome;
+        private float preco;
+
+        public int QtdProduto { get => qtdProduto; set => qtdProduto = value; }
+        public string Nome { get => nome; set => nome = value; }
+        public float Preco { get => preco; set => preco = value; }
+    }
+}
