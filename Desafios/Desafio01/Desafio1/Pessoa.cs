b9c7a4499194327f58ccdd52b273fdbdf3ca4317
Eric Posser <eric_alegrete2002@hotmail.com>
2022-11-02 15:27:01 -0300

    desafio

---
+++ Desafios/Desafio01/Desafio1/Pessoa.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio01
+{
+    public class Pessoa
+    {
+
+        public string nome;
+        public string telefone;
+        public string cidade;
+        public string rg;
+        public string cpf;
+
+
+        public Pessoa(string nome, string telefone, string cidade, string rg, string cpf)
+        {
+            this.nome = nome;
+            this.telefone = telefone;
+            this.cidade = cidade;
+            this.rg = rg;
+            this.cpf = cpf;
+        }
+
+        public Pessoa()
+        {
+        }
+
+    }
+}
+
+
+
+
