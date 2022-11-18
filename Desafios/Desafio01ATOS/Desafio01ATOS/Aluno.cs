a39fb9ff02bceb9a977c447b2f32f3bbcc049dbe
ericposser <eric_alegrete2002@hotmail.com>
2022-11-18 17:10:44 -0300

    atualizacao

---
+++ Desafios/Desafio01ATOS/Desafio01ATOS/Aluno.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio01ATOS
+{
+    public class Aluno : Pessoa
+    {
+        public string matricula;
+        public string codigo;
+        public string nomeCurso;
+        public Aluno()
+        {
+        }
+
+        public Aluno(string nome, string telefone, string cidade, string rg, string cpf) : base(nome, telefone, cidade, rg, cpf)
+        {
+
+            this.nome = nome;
+            this.telefone = telefone;
+            this.cidade = cidade;
+            this.rg = rg;
+            this.cpf = cpf;
+        }
+    }
+}
