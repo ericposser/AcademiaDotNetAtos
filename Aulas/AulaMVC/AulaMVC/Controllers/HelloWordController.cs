5ec7a572ac9a7a9b827e802665f9de41c76dd7f6
ericposser <eric_alegrete2002@hotmail.com>
2022-12-02 14:59:15 -0300

    aula

---
+++ Aulas/AulaMVC/AulaMVC/Controllers/HelloWordController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace AulaMVC.Controllers
+{
+    public class HelloWordController : Controller
+    {
+        public string Index()
+        {
+            return "Minha ação padrão";
+        }
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello" + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
